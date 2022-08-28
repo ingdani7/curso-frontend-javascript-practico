1d5a58d6c9af1b93478952eca18e4748468b8dd8
Daniel Perez <ingdani7@gmail.com>
2022-08-28 12:32:01 -0500

    Implementacion de html con javascript

---
+++ main.js
@@ -0,0 +1,99 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
+
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoAside);
+
+function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    const isAsideClosed =aside.classList.contains('inactive');
+    if (!isAsideClosed){
+       aside.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+    const isMobileMenu = mobileMenu.classList.contains('inactive');
+    const isDesktopMenu = desktopMenu.classList.contains('inactive');
+    if (!isDesktopMenu){
+        desktopMenu.classList.add('inactive');
+    }
+    if (!isMobileMenu){
+        mobileMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
+}
+
+const productList = [];
+productList.push({
+    name: 'Iphone 12 Pro Max',
+    price: 1200,
+    image :'https://images.pexels.com/photos/12794493/pexels-photo-12794493.jpeg?cs=srgb&dl=pexels-geometric-photography-12794493.jpg&fm=jpg',
+})
+productList.push({
+    name: 'Macbook Pro',
+    price: 1350,
+    image :'https://images.pexels.com/photos/303383/pexels-photo-303383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+})
+productList.push({
+    name: 'Airpods',
+    price: 200,
+    image :'https://images.pexels.com/photos/3825517/pexels-photo-3825517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+})
+
+function renderProducts(arr){
+    for (product of productList){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src',product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCard = document.createElement('img');
+        productImgCard.setAttribute('src','./icons/bt_add_to_cart.svg');
+    
+    
+        productInfoFigure.appendChild(productImgCard);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+        
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+        
+        cardsContainer.appendChild(productCard);
+    }
+    
+}
+renderProducts(productList);
